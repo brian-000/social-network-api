1132f30b23434e62c92cd9842e3f121f3b99ee91
Brain <ortiz_brian323@yahoo.com>
2022-06-24 17:55:22 -0700

    added thoughtText column

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,13 @@
 //Thought Model
+const {Schema, model, Types} = require ('mogoose');
+
+const ThoughtScehma = new Schema ({
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 200
+    }
+})
 
 //Reaction sub document
